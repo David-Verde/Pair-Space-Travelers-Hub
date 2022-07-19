e2bb48bee1c569ec9756f591514fcfd36bb85ab7
Unyielding1 <david.verde.alvarez@gmail.com>
2022-07-19 13:46:26 -0500

    changes to app.js to use less code

--- src/App.js
+++ src/App.js
@@ -1,31 +1,21 @@
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from 'react-router-dom';
-import { Provider } from 'react-redux';
-import Header from './Components/Header/header';
+/* eslint-disable no-unused-vars */
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Header from './Components/Header/Header';
+import Rockets from './Components/Rockets/Rockets';
+import Profile from './Components/Profile';
 import store from './configureStore';
-import Rocket from './Components/Rockets/Rocket';
 
 function App() {
   return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <div>
-          <Header />
-
-          <hr />
-          <div className="content">
-            <Routes>
-              <Route path="/" element={<>Missions</>} />
-              <Route path="/Rockets" element={<>Rockets</>} />
-              <Route path="/Profile" element={<>Profile</>} />
-            </Routes>
-          </div>
-        </div>
-      </BrowserRouter>
-    </Provider>
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Rockets />} />
+        <Route path="profile" element={<Profile />} />
+      </Routes>
+    </div>
   );
 }
+
 export default App;
